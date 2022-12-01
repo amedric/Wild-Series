0c3bc47de91dc358736525a025ecf79f00a59b9b
amedric <andyricana@hotmail.com>
2022-12-01 09:05:33 +0100

    push Symfony Quest 10

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+
+class  CategoryController extends AbstractController
+{
+    #[Route('/category/', name: 'category_index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{categoryName}', name: 'category_show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository):Response
+    {
+        $category = $categoryRepository->findBy(['name' => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$categoryName.' found in category\'s table.'
+            );
+        }
+
+        $programs = $programRepository->findby(['category' => $category], ['id' => 'DESC'], limit:3);
+
+        return $this->render('category/show.html.twig', [
+            'programs' => $programs,
+            'category' => $category,
+        ]);
+    }
+}
